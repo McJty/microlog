7bb998f427822d2fbdd69b9bb69d1cef15e4c714
McJty <mcjty1@gmail.com>
2017-09-07 14:51:58 +0200

    Some fixes

--- src/main/java/mcjty/microlog/blocks/BlockBundle.java
+++ src/main/java/mcjty/microlog/blocks/BlockBundle.java
@@ -4,6 +4,10 @@ import mcjty.lib.container.GenericBlock;
 import mcjty.microlog.MicroLogistics;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.util.math.AxisAlignedBB;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.IBlockAccess;
 
 public class BlockBundle extends GenericBlock {
 
@@ -13,6 +17,14 @@ public class BlockBundle extends GenericBlock {
         setSoundType(SoundType.METAL);
         setHarvestLevel("pickaxe", 0);
         setCreativeTab(MicroLogistics.tabMicroLog);
+    }
+
+    public static final AxisAlignedBB HALF_BLOCK_AABB = new AxisAlignedBB(0.0D, 0.0D, 0.0D, 0.5D, 0.5D, 0.5D);
+
+
+    @Override
+    public AxisAlignedBB getBoundingBox(IBlockState state, IBlockAccess source, BlockPos pos) {
+        return HALF_BLOCK_AABB;
     }
 
     @Override
--- src/main/java/mcjty/microlog/mcmp/BundleMultiPart.java
+++ src/main/java/mcjty/microlog/mcmp/BundleMultiPart.java
@@ -2,11 +2,13 @@ package mcjty.microlog.mcmp;
 
 import mcjty.microlog.blocks.ModBlocks;
 import mcmultipart.api.multipart.IMultipart;
+import mcmultipart.api.multipart.IMultipartTile;
 import mcmultipart.api.slot.EnumCornerSlot;
 import mcmultipart.api.slot.IPartSlot;
 import net.minecraft.block.Block;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.IBlockAccess;
@@ -17,6 +19,11 @@ public class BundleMultiPart implements IMultipart {
     @Override
     public Block getBlock() {
         return ModBlocks.blockBundle;
+    }
+
+    @Override
+    public IMultipartTile convertToMultipartTile(TileEntity tileEntity) {
+        return IMultipartTile.wrap(tileEntity);
     }
 
     @Override
